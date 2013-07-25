f27ca0f13372b8c0cbf51a4efba279d1e8617bcc
Xinyue Lu <luxi@onid.orst.edu>
2013-07-25 03:49:38 -0700

    Add ajax support for partially refreshing the room-list by date.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,3 +1,33 @@
 $(function() {
-	$('#datepicker').Calendar({'weekStart': 7});
+	$('#datepicker')
+		.Calendar({'weekStart': 7})
+		.on('changeDay', function(event){
+			load_day(event.year, event.month, event.day);
+		});
+	init_bar_tooltip();
 });
+
+function init_bar_tooltip() {
+	$('div.progress div.bar').tooltip({
+		placement: 'right',
+		container: 'body'
+	})
+}
+
+function load_day(year, month, day) {
+	$.cookie('year', year, { expires: 30 })
+	$.cookie('month', month, { expires: 30 })
+	$.cookie('day', day, { expires: 30 })
+	$('#loading-spinner').fadeIn();
+	$.get('?ajax', function (data) {
+		var new_room_list = $(data);
+		for (var i = 0; i < new_room_list.length; i++) {
+			var div = $(new_room_list[i]);
+			var id = div.attr('id');
+			var html = div.html();
+			$('#' + id).html(html);
+		};
+		init_bar_tooltip();
+		$('#loading-spinner').fadeOut();
+	});
+}
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,21 @@
 class HomeController < ApplicationController
+  layout Proc.new { |controller| controller.request.xhr? ? nil : "application" }
   def index
+    year = cookies[:year]
+    month = cookies[:month]
+    day = cookies[:day]
+    if year.blank? || month.blank? || day.blank?
+      the_day = Time.current
+    else
+      the_day = Time.new year.to_i, month.to_i, day.to_i
+    end
+
     @floors = [1, 2, 5, 6]
     @rooms = Room.all
-    @rooms.each { |room| room.load_reservations_today Time.current }
+    @rooms.each { |room| room.load_reservations_today the_day }
+
+    if request.xhr?
+      render :partial => 'room-list'
+    end
   end
 end
