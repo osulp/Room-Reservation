357e77c7901af57bff8e24b098b4d9a3002bd1b7
Xinyue Lu <luxi@onid.orst.edu>
2013-07-24 20:01:07 -0700

    Add some dummy data to demo the front page views. Add methods to load reservations and render them in the views.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   def index
     @floors = [1, 2, 5, 6]
     @rooms = Room.all
-
+    @rooms.each { |room| room.load_reservations_today Time.current }
   end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,34 @@
 class Reservation < ActiveRecord::Base
   belongs_to :room
-    attr_accessible :description, :end_time, :reserver_onid, :start_time, :user_onid
-    validates :description, :end_time, :start_time, :reserver_onid, :user_onid, presence: true
-    #validates :user_onid, :reserver_onid, numericality: {only_integer: true}
+  attr_accessible :description, :end_time, :reserver_onid, :start_time, :user_onid
+  validates :description, :end_time, :start_time, :reserver_onid, :user_onid, presence: true
+
+  def duration
+    self.end_time - self.start_time
+  end
+
+  def bar_length
+    @bar_length ||= convert_to_pixels self.duration
+  end
+
+  def bar_start
+    @bar_start ||= convert_to_pixels self.start_time.seconds_since_midnight
+  end
+
+  def bar_end
+    @bar_end ||= convert_to_pixels self.end_time.seconds_since_midnight
+  end
+
+  def color
+    return 'danger' if self.user_onid == '_schedule' || self.user_onid == '_maintainance'
+    return 'info' if self.user_onid == 'luxi' #current_user.name
+    return 'warning'
+  end
+
+
+  private
+  def convert_to_pixels time
+    time / 180
+  end
+
 end
