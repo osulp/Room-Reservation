42d2bff04700598cdaf05384c2200f753d40bffc
Trey Terrell <trey.terrell@oregonstate.edu>
2013-08-15 12:38:52 -0700

    Added a note regarding caching.

--- app/models/event_manager/reservation_manager.rb
+++ app/models/event_manager/reservation_manager.rb
@@ -1,6 +1,8 @@
 class EventManager::ReservationManager < EventManager::EventManager
 
   def cache_key(start_time, end_time)
+    # TODO: Do some benchmarking when this comes out - it may be more efficient to just blow the whole
+    #       cache away every time a new reservation comes in.
     @cache_item ||= range_reservations(start_time, end_time).order("updated_at DESC").first
     cache_key = "#{self.class}/#{start_time.to_i}/#{end_time.to_i}/#{room.cache_key}"
     cache_key += "/#{@cache_item.cache_key}" if @cache_item
