f7f0937613da73bd60e2d3afe25ef1c5628900a8
Trey Terrell <trey.terrell@oregonstate.edu>
2013-08-16 10:29:50 -0700

    Built a more manual version of cache expiration.

--- app/models/event_manager/reservation_manager.rb
+++ app/models/event_manager/reservation_manager.rb
@@ -1,13 +1,34 @@
 class EventManager::ReservationManager < EventManager::EventManager
 
+  # Expires caches that would have to do with this reservation.
+  def self.expire_cache(reservation)
+    start_date = reservation.start_time.to_date
+    end_date = reservation.end_time.to_date
+    start_date.upto(end_date) do |date|
+      start_time = Time.zone.parse(date.to_s)
+      end_time = Time.zone.parse((date+1.day).to_s)
+      puts "Start time: #{start_time.to_i} End Time: #{end_time.to_i}"
+      key = self.cache_key(start_time, end_time, reservation.room)
+      puts "Expiring #{key}"
+      Rails.cache.delete(key)
+    end
+  end
+
+  def self.cache_key(start_time, end_time, room)
+    "#{self}/#{start_time.to_i}/#{end_time.to_i}/#{room.cache_key}"
+  end
+
+  def self.form_cache_key(start_time, end_time, room)
+    "#{self.cache_key(start_time, end_time, room)}/#{SecureRandom.hex}"
+  end
+
   def cache_key(start_time, end_time)
     # TODO: Do some benchmarking when this comes out - it may be more efficient to just blow the whole
     #       cache away every time a new reservation comes in.
     #@cache_item ||= range_reservations(start_time, end_time).order("updated_at DESC").first
-    @cache_item = room
-    cache_key = "#{self.class}/#{start_time.to_i}/#{end_time.to_i}/#{room.cache_key}"
-    cache_key += "/#{@cache_item.cache_key}" if @cache_item
-    return cache_key
+    @cache_key ||= Rails.cache.fetch(self.class.cache_key(start_time, end_time, room)) do
+      self.class.form_cache_key(start_time, end_time, room)
+    end
   end
 
   def range_reservations(start_time, end_time)
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,10 +1,15 @@
 class Reservation < ActiveRecord::Base
-  belongs_to :room, :touch => true
+  belongs_to :room
+  after_save :expire_caches
   attr_accessible :description, :end_time, :reserver_onid, :start_time, :user_onid
   validates :description, :end_time, :start_time, :reserver_onid, :user_onid, presence: true
 
   def duration
     self.end_time - self.start_time
+  end
+
+  def expire_caches
+    EventManager::ReservationManager.expire_cache(self)
   end
 
 end
