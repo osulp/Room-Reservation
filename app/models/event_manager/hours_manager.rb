525d2ce334c072d4c3839bcde6bc640d5dcd1442
Trey Terrell <trey.terrell@oregonstate.edu>
2013-08-20 10:45:13 -0700

    Hours manager working..

--- app/models/event_manager/hours_manager.rb
+++ app/models/event_manager/hours_manager.rb
@@ -1,14 +1,88 @@
 class EventManager::HoursManager < EventManager::EventManager
   def hours(date)
+    @hours ||= {}
+    return @hours[date] if @hours.has_key?(date)
     result = {}
     self.class.hour_models.each do |hour_model|
-      result = hour_model.time_info(date)[date]
+      result = hour_model.time_info(date)
+      result = result[date] unless result.blank?
       break unless result.blank?
     end
-    return result
+    @hours[date] = result
+    return @hours[date]
+  end
+
+  def get_events
+    date_start = @start_time.to_date
+    date_end = (@end_time-1.minute).to_date
+    all_events = []
+    date_start.upto(date_end) do |date|
+      hours = hours(date)
+      next if hours["open"] == midnight &&  hours["close"] == midnight
+      all_events |= hours_to_events(hours,date)
+    end
+    all_events
+  end
+
+  def cache_key(start_time, end_time)
+    date_start = start_time.to_date
+    date_end = (end_time-1.minute).to_date
+    hours_cache_key = ""
+    date_start.upto(date_end) do |date|
+      hours = hours(date)
+      unless hours.blank?
+        hours_cache_key += "/#{hours["open"]}/#{hours["close"]}"
+      end
+    end
+    "#{self.class}#{hours_cache_key}"
+  end
+
+  def priority
+    1
   end
 
   private
+
+  def midnight
+    "12:00 am"
+  end
+
+  def one
+    "1:00 am"
+  end
+
+  def special
+    "12:15 am"
+  end
+
+  def hours_to_events(hours,date)
+    start_at = date.at_beginning_of_day
+    end_at = (date+1.day).at_beginning_of_day
+    events = []
+    unless hours.blank? || (hours["open"] == one && hours["close"] == one)
+      unless hours["open"] == special
+        start_at = date.at_beginning_of_day
+        end_at = string_to_time(date, hours["open"])
+        events << build_event(start_at, end_at)
+      end
+      unless hours["close"] == special
+        start_at = string_to_time(date, hours["close"])
+        end_at = (date+1.day).at_beginning_of_day
+        events << build_event(start_at, end_at)
+      end
+    else
+      events << build_event(start_at, end_at)
+    end
+    return events
+  end
+
+  def build_event(start_time, end_time)
+    HoursDecorator.new(Event.new(start_time, end_time, priority))
+  end
+
+  def string_to_time(date, time)
+    Time.zone.parse("#{date} #{time}")
+  end
 
   def self.hour_models
     [
--- spec/models/event_manager/hours_manager_spec.rb
+++ spec/models/event_manager/hours_manager_spec.rb
@@ -34,4 +34,83 @@ describe EventManager::HoursManager do
       end
     end
   end
+
+  describe ".events_between" do
+    subject {EventManager::HoursManager.new.events_between(Time.current.midnight, Time.current.tomorrow.midnight)}
+    context "when the hours are 12:00 to 12:00" do
+      before(:each) do
+        Timecop.freeze(Date.new(2013,8,19)) # A Monday
+        @hours = create(:special_hour, open_time: "00:00:00", close_time: "00:00:00")
+      end
+      it "should return no events" do
+        expect(subject).to eq []
+      end
+    end
+    context "when no hours are returned" do
+      it "should return one event that lasts the entire period for that day" do
+        expect(subject.length).to eq 1
+        expect(subject.first.start_time).to eq Time.current.midnight
+        expect(subject.first.end_time).to eq Time.current.tomorrow.midnight
+      end
+    end
+    context "when hours are 1:00 PM to 10:00 PM" do
+      before(:each) do
+        Timecop.freeze(Date.new(2013,8,19)) # A Monday
+        @hours = create(:special_hour, open_time: "13:00:00", close_time: "22:00:00")
+      end
+      it "should return two events" do
+        expect(subject.length).to eq 2
+      end
+      it "should lock up the reservation from midnight to 1:00 PM" do
+        expect(subject.first.start_time).to eq Time.current.midnight
+        expect(subject.first.end_time).to eq Time.current.midnight+13.hours
+      end
+      it "should lock up the schedule from 22:00:00 to midnight" do
+        expect(subject[1].start_time).to eq Time.current.midnight+22.hours
+        expect(subject[1].end_time).to eq Time.current.midnight+24.hours
+      end
+    end
+    # 1:00 AM to 1:00 AM means "Closed"
+    context "when hours are 1:00 AM to 1:00 AM" do
+      before(:each) do
+        Timecop.freeze(Date.new(2013,8,19)) # A Monday
+        @hours = create(:special_hour, open_time: "01:00:00", close_time: "01:00:00")
+      end
+      it "should return one event" do
+        expect(subject.length).to eq 1
+      end
+      it "should close the schedule all day" do
+        expect(subject.first.start_time).to eq Time.current.midnight
+        expect(subject.first.end_time).to eq Time.current.tomorrow.midnight
+      end
+    end
+    # 12:15 am to x means "Closes at x"
+    context "when hours are 12:15 AM to 4:00 PM" do
+      before(:each) do
+        Timecop.freeze(Date.new(2013,8,19)) # A Monday
+        @hours = create(:special_hour, open_time: "00:15:00", close_time: "16:00:00")
+      end
+      it "should return one event" do
+        expect(subject.length).to eq 1
+      end
+      it "should only lock the schedule from 4:00 to midnight" do
+        expect(subject.first.start_time).to eq Time.current.midnight+16.hours
+        expect(subject.first.end_time).to eq Time.current.tomorrow.midnight
+      end
+    end
+    # x to 12:15 AM means "x - no closing"
+    context "when hours are 4:00 PM to 12:15 AM" do
+      before(:each) do
+        Timecop.freeze(Date.new(2013,8,19)) # A Monday
+        @hours = create(:special_hour, open_time: "16:00:00", close_time: "00:15:00")
+      end
+      it "should return one event" do
+        expect(subject.length).to eq 1
+      end
+      it "should only lock the schedule from midnight to 4:00 PM" do
+        expect(subject.first.start_time).to eq Time.current.midnight
+        expect(subject.first.end_time).to eq Time.current.midnight+16.hours
+      end
+    end
+  end
 end
