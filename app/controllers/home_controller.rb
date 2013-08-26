2952503ed04e896bb96b03f156cbb9e507506981
Trey Terrell <trey.terrell@oregonstate.edu>
2013-08-26 10:28:26 -0700

    Revert "Some profiling.."
    This reverts commit 4f3b51d0590e1a0a90b79c71a438310767ffcb85.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,16 +7,11 @@ class HomeController < ApplicationController
   end
 
   def day
-    Rack::MiniProfiler.step "Calendar Manager" do
-      date = params[:date].split("-")
-      raise "Invalid date given" if date.length != 3
-      calendar_hash = {:year => date[0], :month => date[1], :day => date[2]}
-      calendar = CalendarManager.new(calendar_hash)
-      Rack::MiniProfiler.step "Presenter Generation" do
-        @presenter = CalendarPresenter.cached(calendar.day.midnight, calendar.day.tomorrow.midnight)
-        @presenter.event_collection
-      end
-    end
+    date = params[:date].split("-")
+    raise "Invalid date given" if date.length != 3
+    calendar_hash = {:year => date[0], :month => date[1], :day => date[2]}
+    calendar = CalendarManager.new(calendar_hash)
+    @presenter = CalendarPresenter.cached(calendar.day.midnight, calendar.day.tomorrow.midnight)
     render :partial => 'room_list', :locals => {:floors => @floors}
   end
 end
