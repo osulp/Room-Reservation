68c545caa9214375bc251adb311b69d25286ab7f
Trey Terrell <trey.terrell@oregonstate.edu>
2013-09-06 08:20:23 -0700

    Embedded the max reservation time in the HTML user span.

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -4,10 +4,18 @@ class UserDecorator < Draper::Decorator
   def tag
     h.content_tag(:span,'',:id => "user-info", :data => data_hash)
   end
+
+  # TODO: Change this.
+  def max_reservation_time
+    6.hours
+  end
+
   private
 
   def data_hash
-    {:onid => onid}
+    {:onid => onid,
+     :max_reservation => self.max_reservation_time
+    }
   end
 
 end
