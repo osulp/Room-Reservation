9cccd41482827c70780169ebc9af8ad3e44c2bf8
Trey Terrell <trey.terrell@oregonstate.edu>
2013-09-09 11:35:58 -0700

    Moved default reservation time to configuration.

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -8,14 +8,19 @@ class UserDecorator < Draper::Decorator
   # TODO: Change this.
   def max_reservation_time
     return @max_reservation_time if @max_reservation_time
-    @max_reservation_time = 3.hours
+    @max_reservation_time = default_reservation_time
     if banner_record && banner_record.status
-      @max_reservation_time = reservation_times[banner_record.status.downcase].to_i*60 if reservation_times.has_key?(banner_record.status.downcase)
+      @max_reservation_time = reservation_times[banner_record.status.downcase] if reservation_times.has_key?(banner_record.status.downcase)
     end
+    @max_reservation_time = @max_reservation_time.to_i*60
     return @max_reservation_time
   end
 
   private
+
+  def default_reservation_time
+    APP_CONFIG["users"]["reservation_times"]["default"]
+  end
 
   def reservation_times
     APP_CONFIG["users"]["reservation_times"]
