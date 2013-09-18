46f80f7903e0c0445926bb4b1b9f1fc0d75da746
Trey Terrell <trey.terrell@oregonstate.edu>
2013-09-18 10:53:18 -0700

    Added clean deploy configuration.

---
+++ config/deploy.rb
@@ -0,0 +1,38 @@
+require 'yaml'
+
+config = YAML.load_file('config/config.yml')['deployment'] || {}
+
+require 'bundler/capistrano'
+
+set :stages, config['stages'] || []
+set :default_stage, config['default_stage'] || (config['stages'] || []).first
+require 'capistrano/ext/multistage'
+
+set :application, 'RoomReservation'
+set :repository, config['repository']
+set :user, config['user']
+set :default_environment, config['default_environment'] || {}
+default_run_options[:pty] = true
+set :scm, :git
+set :branch, config['branch']
+set :deploy_via, :remote_cache
+set :deploy_to, config['deploy_to']
+set :use_sudo, false
+set :keep_releases, 5
+set :shared_children, shared_children + %w{pids sockets tmp}
+
+# if you want to clean up old releases on each deploy uncomment this:
+after 'deploy:restart', 'deploy:cleanup'
+
+after 'deploy:update_code', 'deploy:migrate'
+after 'deploy:restart', 'deploy:cleanup'
+after 'deploy:update_code', 'deploy:symlink_config'
+
+namespace :deploy do
+  desc "Symlinks required configuration files"
+  task :symlink_config, :roles => :app do
+    %w{config.yml unicorn.rb god.conf}.each do |config_file|
+      run "ln -nfs #{deploy_to}/shared/config/#{config_file} #{release_path}/config/#{config_file}"
+    end
+  end
+end
