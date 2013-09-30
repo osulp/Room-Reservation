7b13431871e7f52f72e4531f1487fbd486aa19d0
Trey Terrell <trey.terrell@oregonstate.edu>
2013-09-30 09:27:45 -0700

    Added some documentation to the heavy decorated_events method.

--- app/decorators/room_decorator.rb
+++ app/decorators/room_decorator.rb
@@ -10,11 +10,16 @@ class RoomDecorator < Draper::Decorator
     string.strip
   end
 
+
+  # This returns the room's events but with filler "available" events to allow for
+  # easier iteration in the views.
+  # @TODO Refactor this into something more readable.
   def decorated_events(base_time)
     return @decorated_events if @decorated_events
     @decorated_events = []
     last_start_time = base_time.midnight.seconds_since_midnight
     events.each do |event|
+      # Prefix with an 'available' event if required.
       if event.start_time.seconds_since_midnight > last_start_time
         start_time = event.start_time.midnight+last_start_time.seconds
         end_time = event.start_time
@@ -23,6 +28,7 @@ class RoomDecorator < Draper::Decorator
       last_start_time = event.end_time.seconds_since_midnight
       @decorated_events << event
     end
+    # Suffix with an 'available' event if necessary
     if events.length == 0 || last_start_time != 0
       start_time = base_time.midnight+last_start_time.seconds
       end_time = base_time.tomorrow.midnight
