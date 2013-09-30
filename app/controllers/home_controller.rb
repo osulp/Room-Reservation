1e6bc9b9d633720158f4dbd11782236d356a4932
Trey Terrell <trey.terrell@oregonstate.edu>
2013-09-30 07:49:18 -0700

    Moved date parsing down to the CalendarManager.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,16 +2,12 @@ class HomeController < ApplicationController
   before_filter RubyCAS::GatewayFilter, :only => :index
   def index
     calendar = CalendarManager.new(cookies)
-    @time_range = (calendar.day.midnight..calendar.day.tomorrow.midnight)
     @presenter = CalendarPresenter.cached(calendar.day.midnight, calendar.day.tomorrow.midnight)
     @reservation = Reservation.new(:user_onid => current_user.onid, :reserver_onid => current_user.onid)
   end
 
   def day
-    date = params[:date].split("-")
-    raise "Invalid date given" if date.length != 3
-    calendar_hash = {:year => date[0], :month => date[1], :day => date[2]}
-    calendar = CalendarManager.new(calendar_hash)
+    calendar = CalendarManager.from_string(params[:date])
     @presenter = CalendarPresenter.cached(calendar.day.midnight, calendar.day.tomorrow.midnight)
     render :partial => 'room_list', :locals => {:floors => @floors}
   end
