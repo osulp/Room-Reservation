79821b94e7923d84ca3abcc43b14f38550d104a9
Trey Terrell <trey.terrell@oregonstate.edu>
2013-10-03 16:21:08 -0700

    Removed deprecated config options.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,10 +66,6 @@ RoomReservation::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
   config.action_mailer.default_url_option = { host: 'RoomReservation.com' }
   config.eager_load = true
 end
