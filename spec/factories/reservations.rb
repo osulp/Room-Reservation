380d2a7039b95cce3c65ff3327f8cdbf91eaf341
Xinyue Lu <luxi@onid.orst.edu>
2013-11-19 14:57:18 -0800

    Add specs for key cards admin panel

--- spec/factories/reservations.rb
+++ spec/factories/reservations.rb
@@ -2,9 +2,9 @@
 
 FactoryGirl.define do
   factory :reservation do
-    user_onid 1
+    sequence(:user_onid) {|n| "user#{n}"}
     room
-    reserver_onid 1
+    sequence(:reserver_onid) {|n| "user#{n}"}
     start_time {Time.now}
     end_time {Time.now}
     description "MyString"
---
+++ spec/features/admin/key_card_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+describe "key_card administration" do
+  let(:user) {build(:user, :admin)}
+  let(:room) {create(:room)}
+  let(:key_card) {create(:key_card)}
+  before(:each) do
+    RubyCAS::Filter.fake(user.onid)
+    room
+    key_card
+    visit admin_key_cards_path
+  end
+  context "when there are keycards defined" do
+    it "should show them" do
+      within(".admin-container") do
+        expect(page).to have_content(key_card.key)
+      end
+    end
+    it "should let you edit a key card" do
+      click_link "Edit"
+      fill_in "key_card_key", :with => "87654321"
+      choose room.name
+      click_button "Save"
+      expect(page).to have_content("Key Card updated")
+      expect(KeyCard.last.key).to eq 87654321
+      expect(KeyCard.last.room).to eq room
+    end
+    it "should let you delete a key card", :js => true do
+      expect(page).to have_content(key_card.key)
+      find("a[href='/admin/key_cards/#{key_card.id}'][data-method='delete']").click
+      expect(page).not_to have_content(key_card.key)
+      expect(page).to have_content("Key Card deleted")
+    end
+    context "when key card is checked out" do
+      let(:key_card) {create(:key_card_checked_out)}
+      it "should show the related reservation" do
+        expect(page).to have_content(key_card.reservation.user_onid)
+      end
+    end
+    context "when key card is returned" do
+      it "should show available" do
+        within(".admin-container") do
+          expect(page).to have_content("Available")
+        end
+      end
+    end
+  end
+  it "should let you create a key card" do
+    click_link "New KeyCard"
+    fill_in "key_card_key", :with => "87654321"
+    choose room.name
+    click_button "Create"
+    expect(page).to have_content("Key Card added")
+    expect(KeyCard.last.key).to eq 87654321
+    expect(KeyCard.last.room).to eq room
+  end
+end
