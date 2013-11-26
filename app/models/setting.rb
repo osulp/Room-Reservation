7f4c480b44ff179accf8c7ccc5b97f669d7816b0
Trey Terrell <trey.terrell@oregonstate.edu>
2013-11-26 09:37:04 -0800

    Moved out patches to druthers.

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,38 +1,9 @@
 class Setting < ActiveRecord::Base
-  serialize :value
   # Valid setting keys
   VALID_KEYS = [:max_concurrent_reservations, :announcement_header_message, :day_limit]
   NUMERIC_KEYS = [:max_concurrent_reservations, :day_limit]
-
-  def self.valid_keys
-    VALID_KEYS
-  end
-
-  def self.druthers_cache
-    Rails.cache
-  end
-
-  def self.set_druther(key, value)
-    obj = where(key: key).first_or_initialize
-    if obj.respond_to? :update!
-      # Rails 4.x:
-      obj.update!(value: value)
-    else
-      # Rails 3.x:
-      obj.update_attributes!(value: value)
-    end
-    # Only update the cache if the update! succeeded:
-    druthers_cache.write("#{self}/#{key}", value)
-    obj
-  end
-  def self.get_druther(key)
-    druthers_cache.fetch("#{self}/#{key}") do
-      val = where(key: key).pluck(:value).to_a
-      val.present? ? val.first : send_druthers_event(:default, key)
-    end
-  end
-
-  def_druthers(*VALID_KEYS)
+  include DruthersPatch
+  serialize :value
 
   def self.default_max_concurrent_reservations
     0
@@ -44,12 +15,6 @@ class Setting < ActiveRecord::Base
 
   def self.default_day_limit
     0
-  end
-
-  NUMERIC_KEYS.each do |key|
-    define_method("validate_#{key}") do
-      numerical_validation
-    end
   end
 
   # @return [String] Friendly name of this asset (looked up in locale)
---
+++ lib/druthers_patch.rb
@@ -0,0 +1,40 @@
+module DruthersPatch
+  extend ActiveSupport::Concern
+  included do
+    def_druthers(*self.const_get(:VALID_KEYS))
+
+    self.const_get(:NUMERIC_KEYS).each do |key|
+      define_method("validate_#{key}") do
+        numerical_validation
+      end
+    end
+  end
+  module ClassMethods
+    def valid_keys
+      self.const_get(:VALID_KEYS)
+    end
+
+    def druthers_cache
+      Rails.cache
+    end
+    def set_druther(key, value)
+      obj = where(key: key).first_or_initialize
+      if obj.respond_to? :update!
+        # Rails 4.x:
+        obj.update!(value: value)
+      else
+        # Rails 3.x:
+        obj.update_attributes!(value: value)
+      end
+      # Only update the cache if the update! succeeded:
+      druthers_cache.write("#{self}/#{key}", value)
+      obj
+    end
+    def get_druther(key)
+      druthers_cache.fetch("#{self}/#{key}") do
+        val = where(key: key).pluck(:value).to_a
+        val.present? ? val.first : send_druthers_event(:default, key)
+      end
+    end
+  end
+end
