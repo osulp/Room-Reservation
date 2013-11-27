13078df771025b502c42e73d68b66b1bfd5752bf
Trey Terrell <trey.terrell@oregonstate.edu>
2013-11-27 08:54:36 -0800

    Increased timeout.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,6 +3,7 @@ require 'capybara/rspec'
 require 'capybara/poltergeist'
 require 'capybara-screenshot/rspec'
 Capybara.javascript_driver = :poltergeist
+Capybara.default_wait_time = 5
 Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(app, {:timeout => 60})
 end
