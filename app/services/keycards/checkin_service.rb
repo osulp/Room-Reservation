bce29234b4226534b3ca05121328d55dd109be9d
Trey Terrell <trey.terrell@oregonstate.edu>
2013-12-17 10:30:43 -0800

    Added checkin service.

--- app/services/keycards/checkin_service.rb
+++ app/services/keycards/checkin_service.rb
@@ -2,6 +2,8 @@ class Keycards::CheckinService
   include ActiveModel::Model
   attr_accessor :keycard, :user
   validate :user_can_checkin
+  validate :append_keycard_errors
+  validate :append_reservation_errors
 
   def initialize(keycard, user)
     self.keycard = keycard
@@ -16,11 +18,46 @@ class Keycards::CheckinService
   end
 
   def save
+    truncate_reservation
+    reservation = keycard.reservation
+    remove_reservation
     return false unless valid?
-    true
+    KeyCard.transaction do
+      result = keycard.save
+      result &&= Reserver.new(reservation).save if reservation
+      raise ActiveRecord::Rollback unless result == true
+    end
+    return true
   end
 
   private
+
+  # @TODO: Add truncated_at timestamp.
+  def truncate_reservation
+    keycard.reservation.end_time = Time.current if keycard.reservation
+  end
+
+  def remove_reservation
+    keycard.reservation = nil
+  end
+
+  def append_keycard_errors
+    return if !keycard
+    unless keycard.valid?
+      keycard.errors.full_messages.each do |msg|
+        self.errors.add(:base, msg)
+      end
+    end
+  end
+
+  def append_reservation_errors
+    return if !keycard.reservation
+    unless keycard.reservation.valid?
+      keycard.reservation.errors.full_messages.each do |msg|
+        self.errors.add(:base, msg)
+      end
+    end
+  end
 
   def user_can_checkin
     errors.add(:base, "You are unauthorized to check in a key card.") unless user_ability.can?(:check_in, keycard)
--- spec/services/keycards/checkin_service_spec.rb
+++ spec/services/keycards/checkin_service_spec.rb
@@ -4,11 +4,24 @@ describe Keycards::CheckinService do
   let(:keycard) {create(:key_card)}
   let(:user) {build(:user, :staff)}
   subject {Keycards::CheckinService.new(keycard, user)}
+  before(:each) do
+    Timecop.freeze
+    create(:special_hour, start_date: 60.days.ago, end_date: 60.days.from_now, open_time: '00:00:00', close_time: '00:00:00')
+  end
   it "should be valid" do
     expect(subject).to be_valid
   end
   context "when the user is not staff" do
     let(:user) {build(:user)}
+    it "should be invalid" do
+      expect(subject).not_to be_valid
+    end
+  end
+  context "when the reservation is invalid" do
+    let(:keycard) {create(:key_card_checked_out)}
+    before(:each) do
+      keycard.reservation.user_onid = ""
+    end
     it "should be invalid" do
       expect(subject).not_to be_valid
     end
@@ -23,9 +36,6 @@ describe Keycards::CheckinService do
       end
     end
     context "when the keycard has a reservation" do
-      before(:each) do
-        Timecop.freeze
-      end
       let(:keycard) {create(:key_card_checked_out, :reservation => reservation)}
       let(:reservation) {create(:reservation, :user_onid => user.onid, :reserver_onid => user.onid, :start_time => start_time, :end_time => end_time)}
       let(:start_time) {Time.current - 2.hours}
@@ -35,7 +45,7 @@ describe Keycards::CheckinService do
           expect(@result).to eq true
         end
         it "should truncate the reservation" do
-          expect(Reservation.first.end_time).to eq Time.current
+          expect(Reservation.first.end_time.to_i).to eq Time.current.to_i
         end
       end
     end
