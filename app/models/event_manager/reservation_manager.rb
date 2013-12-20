d115ceff8858d02a4d76777f76b11f3c3e9b6212
Trey Terrell <trey.terrell@oregonstate.edu>
2013-12-20 15:19:23 -0800

    Added tests for the truncated_at effect on events.

--- app/models/event_manager/reservation_manager.rb
+++ app/models/event_manager/reservation_manager.rb
@@ -18,7 +18,7 @@ class EventManager::ReservationManager < EventManager::EventManager
   end
 
   def to_event(reservation)
-    end_time = reservation.truncated_at if reservation.truncated_at && reservation.truncated_at < reservation.end_time
+    end_time = reservation.truncated_at if reservation.truncated_at && reservation.truncated_at < reservation.end_time && reservation.truncated_at >= reservation.start_time
     end_time ||= reservation.end_time
     ReservationDecorator.new(Event.new(reservation.start_time-reservation_padding, end_time+reservation_padding, priority, reservation, reservation.room_id))
   end
