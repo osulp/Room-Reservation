351ed336b3c78e8f632e39b77b833e73721b6f6e
Trey Terrell <trey.terrell@oregonstate.edu>
2013-12-26 15:08:08 -0800

    Made room names display floor shortcuts if a whole floor is chosen.

--- app/decorators/cleaning_record_decorator.rb
+++ app/decorators/cleaning_record_decorator.rb
@@ -17,7 +17,24 @@ class CleaningRecordDecorator < Draper::Decorator
     all_rooms.map{|x| x.floor.to_i}.uniq.sort.map{|x| ["Floor #{x}", "floor_#{x}", x]}
   end
 
+  def room_names
+    room_names = []
+    rooms.group_by(&:floor).each do |floor, floor_rooms|
+      mapped_names = floor_rooms.map(&:name)
+      if floor_rooms.map{|x| x.name} == organized_floors[floor]
+        room_names << "Floor #{floor}"
+      else
+        room_names |= mapped_names
+      end
+    end
+    room_names
+  end
+
   private
+
+  def organized_floors
+    @organized_floors ||= Room.all.order(:name).pluck(:name, :floor).group_by{|x| x[1]}.each_with_object({}) {|(key, value), hsh| hsh[key] = value.map{|x| x[0]}}
+  end
 
   def all_rooms
     @all_rooms ||= Room.all
