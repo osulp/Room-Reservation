95fbc733b9ca9bab412482d1dde2379ed534160b
Trey Terrell <trey.terrell@oregonstate.edu>
2014-02-10 17:33:25 -0800

    Removed commented code.

--- app/controllers/admin/logs_controller.rb
+++ app/controllers/admin/logs_controller.rb
@@ -1,6 +1,5 @@
 class Admin::LogsController < AdminController
   def index
     @presenter = LogsPresenter.new(self,params)
-    #@reservations = AdminReservationsDecorator.new(filtered_reservations.page(params[:page]).per(per_page))
   end
 end
