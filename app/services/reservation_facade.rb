025665655e220849508f576945159e83e3dd581a
Trey Terrell <trey.terrell@oregonstate.edu>
2014-03-05 15:45:23 -0800

    Added overdue notification.

--- app/services/reservation_facade.rb
+++ app/services/reservation_facade.rb
@@ -8,8 +8,8 @@ class ReservationFacade
   def reservations
     # Split all reservations into coming ones and invalid ones
     reservations = []
-    reservations[0] = decorator.decorate_collection(user.reservations.with_deleted.where("end_time >= ? AND truncated_at IS NULL AND deleted_at IS NULL", Time.current).order(:start_time))
-    reservations[1] = decorator.decorate_collection(user.reservations.with_deleted.where("end_time < ? OR truncated_at IS NOT NULL OR deleted_at IS NOT NULL", Time.current).order("start_time DESC"))
+    reservations[0] = decorator.decorate_collection(user.reservations.with_deleted.includes(:key_card).references(:key_cards).where("(key_cards.id IS NOT NULL AND end_time < ?) OR (end_time >= ? AND truncated_at IS NULL AND deleted_at IS NULL)", Time.current, Time.current).order(:start_time))
+    reservations[1] = decorator.decorate_collection(user.reservations.with_deleted.includes(:key_card).references(:key_cards).where("(key_cards.id IS NULL OR end_time >= ?) AND (end_time < ? OR truncated_at IS NOT NULL OR deleted_at IS NOT NULL)", Time.current, Time.current).order("start_time DESC"))
     #reservations = user.reservations.with_deleted.order(:start_time).partition do |r|
     #  r.end_time.future? && !r.deleted? && r.truncated_at.blank?
     #end
