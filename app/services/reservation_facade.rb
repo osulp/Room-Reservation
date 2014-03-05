7ac792ed4ee339d461a7b6fed6a4e2d315664c14
Trey Terrell <trey.terrell@oregonstate.edu>
2014-03-05 14:40:49 -0800

    Converted reservation facade to use SQL instead of ruby.

--- app/services/reservation_facade.rb
+++ app/services/reservation_facade.rb
@@ -7,12 +7,16 @@ class ReservationFacade
 
   def reservations
     # Split all reservations into coming ones and invalid ones
-    reservations = user.reservations.with_deleted.order(:start_time).partition do |r|
-      r.end_time.future? && !r.deleted? && r.truncated_at.blank?
-    end
+    reservations = []
+    reservations[0] = decorator.decorate_collection(user.reservations.with_deleted.where("end_time >= ? AND truncated_at IS NULL AND deleted_at IS NULL", Time.current).order(:start_time).decorate)
+    reservations[1] = decorator.decorate_collection(user.reservations.with_deleted.where("end_time < ? OR truncated_at IS NOT NULL OR deleted_at IS NOT NULL", Time.current).order("start_time DESC"))
+    #reservations = user.reservations.with_deleted.order(:start_time).partition do |r|
+    #  r.end_time.future? && !r.deleted? && r.truncated_at.blank?
+    #end
     # TODO: Cache this if necessary; Slice this if it's too long
-    reservations[1].reverse!
-    return reservations.map{|x| x.map{|y| decorator.new(y)}}
+    #reservations[1].reverse!
+    #return reservations.map{|x| x.map{|y| decorator.new(y)}}
+    return reservations
   end
 
   private
