d2781040564607a4c315b3e923ac347f06eb8e80
Trey Terrell <trey.terrell@oregonstate.edu>
2014-03-22 00:13:31 -0700

    Work on free times.

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -9,7 +9,8 @@ class RoomsController < ApplicationController
       time = Time.zone.parse(date.to_s)
       presenter = CalendarPresenter.cached(time.midnight, time.tomorrow.midnight)
       presenter.rooms.each do |room|
-        hash[room.name] ||= {date.to_s => []}
+        hash[room.name] ||= {}
+        hash[room.name][date.to_s] = []
         hash[room.name]["image"] = room.image.url unless room.image.blank?
         hash[room.name]["image"] ||= ""
         room.decorated_events(time).each do |event|
