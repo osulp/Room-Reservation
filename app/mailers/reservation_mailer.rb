54f0190924d9cb160bc34c585b71638a876ea289
Xinyue Lu <luxi@onid.orst.edu>
2014-03-28 14:57:37 -0700

    Update mailer and corresponding testing code

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -3,7 +3,7 @@ class ReservationMailer < ActionMailer::Base
     @user = user
     @reservation = reservation
     @footer = Setting.reservation_email
-    mail(:to => @user.email, :from => from_email, :reply_to => from_email, :subject => reservation_subject)
+    mail(:to => @user.email, :from => from_email, :reply_to => from_email, :subject => Setting.email_reservation_subject)
   end
 
   def update_email(reservation, user)
@@ -11,29 +11,17 @@ class ReservationMailer < ActionMailer::Base
     @reservation = reservation
     @previous = @reservation.previous_version
     @footer = Setting.update_email
-    mail(:to => @user.email, :from => from_email, :reply_to => from_email, :subject => update_subject)
+    mail(:to => @user.email, :from => from_email, :reply_to => from_email, :subject => Setting.email_update_subject)
   end
 
   def cancellation_email(reservation, user)
     @user = user
     @reservation = reservation
     @footer = Setting.cancellation_email
-    mail(:to => @user.email, :from => from_email, :reply_to => from_email, :subject => cancellation_subject)
+    mail(:to => @user.email, :from => from_email, :reply_to => from_email, :subject => Setting.email_cancellation_subject)
   end
 
   private
-
-  def reservation_subject
-    "Study Room Reservations Reservation"
-  end
-
-  def cancellation_subject
-    "Study Room Reservations Cancellation"
-  end
-
-  def update_subject
-    "Study Room Reservation Updated"
-  end
 
   def from_email
     "#{Setting.from_name} <#{Setting.from_address}>"
