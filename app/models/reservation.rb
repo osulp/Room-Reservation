a96746b2a1b55a02145bda5380db07e83b270b35
Trey Terrell <trey.terrell@oregonstate.edu>
2014-04-08 16:49:10 -0700

    Added direct link to banner record for reservation.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,6 +3,7 @@ class Reservation < ActiveRecord::Base
   has_paper_trail
   belongs_to :room
   has_one :key_card
+  has_one :user_banner_record, :class_name => "BannerRecord", :foreign_key => "onid", :primary_key => "user_onid"
   before_destroy :touch
   validates :end_time, :start_time, :reserver_onid, :user_onid, :room, presence: true
   validate :not_swearing
