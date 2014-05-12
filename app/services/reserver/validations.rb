cdf8f47aee4147c54934c986aca6a66d14c7a937
Trey Terrell <trey.terrell@oregonstate.edu>
2014-05-12 09:00:40 -0700

    Centralized existence checking logic

--- app/services/reserver/validations.rb
+++ app/services/reserver/validations.rb
@@ -2,65 +2,83 @@ class Reserver
   module Validations
     extend ActiveSupport::Concern
     included do
+      validate :run_validations
       validate :user_not_nil
-      validate :start_time_less_than_end_time
-      validate :reservation_not_in_past
-      validate :room_is_persisted
-      validate :time_is_available
-      validate :duration_correct
-      validate :authorized_to_reserve
-      validate :concurrency_limit
-      validate :day_limit_applied
     end
 
+    def run_validations
+      if required_values_exist?
+        start_time_less_than_end_time
+        reservation_not_in_past
+        room_is_persisted
+        time_is_available
+        duration_correct
+        authorized_to_reserve
+        concurrency_limit
+        day_limit_applied
+      end
+    end
+
+    def required_values_exist?
+      %w{start_time end_time user reserver room user_onid reserver_onid}.map{|x| self.send(x).present?}.inject{|bool, val| bool && val} 
+    end
+
+    def user_not_nil
+      errors.add(:base, "A username must be chosen to reserve for.") if user.nil?
+      errors.add(:base, "Invalid Reserving Party") if reserver.nil?
+    end
+
+
     def day_limit_applied
-      return if !start_time || day_limit == 0 || !reserver || reserver_ignores_restrictions?
+      return if day_limit == 0 || reserver_ignores_restrictions?
       if Time.current+day_limit.days < start_time
         errors.add(:base, "You can only make reservations #{day_limit} days in advance")
       end
     end
 
-    def user_not_nil
-      return if !user || !reserver
-      errors.add(:base, "A username must be chosen to reserve for.") if user.nil?
-      errors.add(:base, "Invalid Reserving Party") if reserver.nil?
-    end
-
     # TODO: Evaluate this - what if they have a reservation on the second day when this crosses midnight?
     def concurrency_limit
-      max_concurrent = Setting.max_concurrent_reservations.to_i
-      return if !user || !reserver || !start_time || max_concurrent == 0 || reserver_ignores_restrictions?
-      current_reservations = user.reservations.where("start_time <= ? AND end_time >= ? AND start_time >= ? AND reserver_onid = ?", start_time.tomorrow.midnight-1.second, start_time.midnight, start_time.midnight, user.onid).size
-      errors.add(:base, "You can only make #{pluralize(max_concurrent, "reservation")} per day.") if current_reservations >= max_concurrent
+      return if max_concurrent == 0 || reserver_ignores_restrictions?
+      errors.add(:base, "You can only make #{pluralize(max_concurrent, "reservation")} per day.") if current_ongoing_reservations.size >= max_concurrent
+    end
+
+    def max_concurrent
+      Setting.max_concurrent_reservations.to_i
+    end
+
+    def current_ongoing_reservations
+      user.reservations.where("start_time <= ? AND end_time >= ? AND start_time >= ? AND reserver_onid = ?", start_time.tomorrow.midnight-1.second, start_time.midnight, start_time.midnight, user.onid)
     end
 
     def authorized_to_reserve
-      return if !user || !reserver || reserver_ignores_restrictions? || !user_onid || !reserver_onid
+      return if reserver_ignores_restrictions?
       errors.add(:base, "You are not authorized to reserve on behalf of #{user.onid}") if user.onid.downcase != reserver.onid.downcase
     end
 
     def duration_correct
-      return if !end_time || !start_time || !user || !reserver || reserver_ignores_restrictions?
-      duration = end_time - start_time
+      return if reserver_ignores_restrictions?
       errors.add(:base, "The reservation can not be for more than #{(user.max_reservation_time/60/60).to_i} hours") if duration > user.max_reservation_time
+    end
+    
+    def duration
+      end_time - start_time
     end
 
     def start_time_less_than_end_time
-      errors.add(:start_time, "must be less than end time") if start_time && end_time && start_time >= end_time
+      errors.add(:start_time, "must be less than end time") if start_time >= end_time
     end
 
     def room_is_persisted
-      errors.add(:base, "The requested room does not exist.") if !room || !room.persisted?
+      errors.add(:base, "The requested room does not exist.") if !room.persisted?
     end
 
     def time_is_available
       checker = AvailabilityChecker.new(room, start_time, end_time, @reservation)
-      return if !room || !start_time || !end_time
       errors.add(:base, "The requested time slot is not available for reservation") unless checker.available?
     end
 
     def reservation_not_in_past
-      return if !start_time || !reserver || reserver_ability.can?(:ignore_restrictions,self)
+      return if reserver_ignores_restrictions?
       errors.add(:base, "You may not make reservations in the past.") unless start_time >= Time.current
     end
 
