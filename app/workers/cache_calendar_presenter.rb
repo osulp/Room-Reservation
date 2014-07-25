1dd9204025ddcea3e1a7c8d155535407c9f3ced4
Trey Terrell <trey.terrell@oregonstate.edu>
2014-07-25 10:09:41 -0700

    Fix error caused by nil ignore_managers

--- app/workers/cache_calendar_presenter.rb
+++ app/workers/cache_calendar_presenter.rb
@@ -3,7 +3,7 @@ class CacheCalendarPresenter
   def perform(start_time, end_time, key, skip_publish, ignore_managers=nil)
     start_time = Time.zone.at(start_time)
     end_time = Time.zone.at(end_time)
-    ignore_managers.map!{|x| x.constantize if x.kind_of?(String)}
+    Array.wrap(ignore_managers).map!{|x| x.constantize if x.kind_of?(String)}
     unless Rails.cache.exist?(key, :deserialize => false)
       presenter = CalendarPresenter.new(start_time, end_time, nil, ignore_managers)
       presenter.rooms
