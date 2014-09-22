c64784b53ff50d2d6826a8600baf67145864cbed
Trey Terrell <trey.terrell@oregonstate.edu>
2014-09-22 15:36:01 -0700

    More sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,7 @@
 Sidekiq.configure_server do |config|
   config.redis = {:url => 'redis://localhost:6379',:namespace => "roomreservation"}
 end
+
+Sidekiq.configure_client do |config|
+  config.redis = {:url => 'redis://localhost:6379',:namespace => "roomreservation"}
+end
