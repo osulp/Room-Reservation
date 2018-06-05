27f52f636a67bcd132f5280f5f78e3f1933af110
luisgreg99 <luisram90_101@hotmail.com>
2018-06-05 16:18:26 -0700

    reset sidekiq initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = {:url => 'redis://redis:6379',:namespace => "roomreservation"}
+  config.redis = {:url => 'redis://localhost:6379',:namespace => "roomreservation"}
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = {:url => 'redis://redis:6379',:namespace => "roomreservation"}
+  config.redis = {:url => 'redis://localhost:6379',:namespace => "roomreservation"}
 end
