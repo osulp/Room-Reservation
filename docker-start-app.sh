a25df508631e54a53594ec87c899e7290096ae65
Josh Gum <revgum@gmail.com>
2018-06-13 08:04:23 -0700

    simplifies docker-compose

--- docker-start-app.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-#rackup faye.ru -s thin -E production &
-rm tmp/pids/*
-RAILS_ENV=development bundle exec rake db:create
-RAILS_ENV=development bundle exec rake db:migrate
-# RAILS_ENV=test bundle exec rake db:create
-# RAILS_ENV=test bundle exec rake db:migrate
-rails s -p 3000 -b "0.0.0.0"
