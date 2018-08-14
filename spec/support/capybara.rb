3d96dbca1f80a104510bfd93d5815489b7a8928e
Josh Gum <revgum@gmail.com>
2018-08-14 14:52:59 -0700

    fixing tests

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,7 +3,7 @@ require 'capybara/rspec'
 require 'capybara/poltergeist'
 require 'capybara-screenshot/rspec'
 Capybara.javascript_driver = :poltergeist
-Capybara.default_wait_time = 10
+Capybara.default_max_wait_time = 10
 Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(app, {:timeout => 60})
 end
