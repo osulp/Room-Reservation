c696785f68756bd929234bdc51389a962ce4cde6
Josh Gum <revgum@gmail.com>
2018-11-02 14:05:46 -0700

    not using devise

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,8 +29,6 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   config.include ::Rails.application.routes.url_helpers
-  config.include Devise::Test::ControllerHelpers, type: :controller
-
   config.include Capybara::RSpecMatchers, :type => :view
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
