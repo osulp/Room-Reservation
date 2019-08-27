8a4524e3f3efc2aeb64d04659182189de571fd98
straleyb <straleyb@onid.oregonstate.edu>
2019-08-27 08:31:42 -0700

    find defs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -47,6 +47,9 @@ RSpec.configure do |config|
   # Focus Settings
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
+  config.before do
+    FactoryBot.find_definitions
+  end
 end
 
 Capybara.javascript_driver = :poltergeist
