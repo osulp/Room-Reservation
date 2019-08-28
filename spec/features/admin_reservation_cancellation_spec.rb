3ed80e63856ecd451a2d6a5a795ad35da371c5a1
straleyb <straleyb@onid.oregonstate.edu>
2019-08-28 07:39:32 -0700

    remove special hour creation

--- spec/features/admin_reservation_cancellation_spec.rb
+++ spec/features/admin_reservation_cancellation_spec.rb
@@ -13,7 +13,7 @@ describe 'cancelling a reservation' do
   end
   before(:each) do
     RubyCAS::Filter.fake(user.onid)
-    create(:special_hour, start_date: Date.yesterday, end_date: Date.tomorrow, open_time: "00:00:00", close_time: "00:00:00")
+    # create(:special_hour, start_date: Date.yesterday, end_date: Date.tomorrow, open_time: "00:00:00", close_time: "00:00:00")
     banner_record
     @room = create(:room)
     @start_time = Time.current+1.hours
