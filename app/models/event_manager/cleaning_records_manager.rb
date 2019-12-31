df2e612457b84a733387a19928d99e4aeff9b2e4
Greg Luis Ramirez <gregorio.luisramirez@oregonstate.edu>
2019-12-31 12:06:23 -0800

    pull additional open/close hour block from hours API

--- app/models/event_manager/cleaning_records_manager.rb
+++ app/models/event_manager/cleaning_records_manager.rb
@@ -5,7 +5,29 @@ class EventManager::CleaningRecordsManager < EventManager::EventManager
   end
 
   def get_events
-    range_cleaning_records(start_time, end_time).map{|x| to_event(x)}.flatten
+    records = range_cleaning_records(start_time, end_time).map{|x| to_event(x)}.flatten
+
+    # Note: after getting the events, add Events::CleaningEvent to fill in the
+    # blocks for multiple closing blocks. Given a start_time date, for each
+    # room in floors 1, 2 and 5, add a CleaningRecord without persisting
+    # it to automatically add a close block for this additional
+    # open/close block. This only works for one additional open/close
+    # block for now.
+    start_date = start_time.strftime('%Y-%m-%d')
+    hours_manager = EventManager::HoursManager.new
+    open_hours = hours_manager.hours(start_date)
+    if open_hours['all_open_hours'].count > 1
+      Room.where(floor: [1,2,5]).each do |room|
+        tmp_close_time = Time.parse(open_hours['all_open_hours'].first['close']).strftime('%H:%M:%S')
+        tmp_open_time = Time.parse(open_hours['all_open_hours'].last['open']).strftime('%H:%M:%S')
+        tmp_start_time = Time.zone.parse("#{start_date} #{tmp_close_time}")
+        tmp_end_time = Time.zone.parse("#{start_date} #{tmp_open_time}")
+        attributes = {id: rand(DateTime.now.to_i), start_date: "#{start_date}", end_date: "#{start_date}", start_time: "#{tmp_start_time}", end_time: "#{tmp_end_time}"}
+        cleaning_record = CleaningRecord.new(attributes)
+        records << Events::CleaningEvent.new(tmp_start_time, tmp_end_time, 0, cleaning_record, room.id)
+      end
+    end
+    records
   end
 
   def cache_key(start_time, end_time,rooms=[])
